64594fd3b3725479ffd82a5b48c0e4780e8b2685
Oudani Achraf <achraf.oudani@gmail.com>
2021-02-01 21:18:45 +0100

    fetch data

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url ='https://covid19.mathdro.id/api';
- 
-export const fetchData = async () =>{
-   try{
-       const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+const url = "https://covid19.mathdro.id/ap";
 
-       return { confirmed, recovered,deaths,lastUpdate,}
-        
-       
-   } catch(error){}
-   
-}
+export const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
 
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {}
+};
